2bad403e64bbf5ce79051be6b0f046615d2805dc
felixacdc <pfmata01@gmail.com>
2017-02-22 10:18:46 -0600

    [add] 02 - Gulp + Sass

--- files/gulpfile.js
+++ files/gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('saludar', () => {
     console.log('Hola mundo');
+});
+
+gulp.task('sass', () => {
+    return gulp.src('scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
 });
